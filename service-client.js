9b66e29765bf883451dcdfb3157ffc1c3ef5a1f3
Doug Tidwell <doug_tidwell@yahoo.com>
2017-10-05 17:54:08 -0400

    Expanded documentation, added metadata to package.json

--- service-client.js
+++ service-client.js
@@ -5,11 +5,17 @@ var options = {
 
   // Use any name you like...
   body: {'name': 'Doug'},
+
+  // We're using JSON, of course. You may need to include Authorization or
+  // other HTTP headers depending on the service. For the service we're using
+  // here, this is all you need. 
   headers: {'Content-Type': 'application/json'},
   json: true
 };
 
-function owCallback(err, response, body) {
+// If the call worked, we print the JSON response, otherwise we print
+// an error message. Simple as that.
+function icfCallback(err, response, body) {
   if (!err && response.statusCode == 200) {
     console.log(response.body.greeting);
   } else {
@@ -17,5 +23,6 @@ function owCallback(err, response, body) {
   }
 }
 
+// We're doing a POST with the options and callback function we defined
 var request = require('request');
-request.post(options, owCallback);
+request.post(options, icfCallback);
--- service.js
+++ service.js
@@ -1,13 +1,18 @@
-// This is the sample function deployed to the IBM Cloud Functions platform
-// via the developerWorks sandbox
+// This is the source of the sample function deployed to the IBM Cloud
+// Functions platform via the developerWorks sandbox
 // (https://developer.ibm.com/sandbox/index.html).
 
+// JSON data comes in, JSON goes out
 function main(params) {
   var greeting = 'Hello, ' + params.name;
   return {greeting};
 }
 
+// Choose a really good name as the default
 var defaultParameters = {'name': 'Doug'};
 
+// require.main === module means this file is being run directly instead of
+// via require(). If this code is run directly, main() is invoked with the
+// default parameters.
 if (require.main === module)
   console.log(main(defaultParameters));
